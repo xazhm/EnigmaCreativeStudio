b9eff2c001b50ee20b9afd432354309a1dbe1734
Muflihsan <muflihihsanpratama@gmail.com>
2023-06-21 18:51:59 +0700

    fix category

--- database/migrations/2023_06_20_114839_create_category_table.php
+++ database/migrations/2023_06_20_114839_create_category_table.php
@@ -11,15 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('designs', function (Blueprint $table) {
+        Schema::create('category', function (Blueprint $table) {
             $table->id();
-            $table->string('gambar');
             $table->string('name');
-            $table->string('detail');
-            $table->integer('harga');
-            $table->timestamps();
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('category');
         });
     }
 
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('designs');
+        Schema::dropIfExists('category');
     }
 };
